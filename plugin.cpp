044ed9081691129e54471cb6b7a3b0d61039a731
Federico Ferri <federico.ferri.it@gmail.com>
2018-06-10 14:28:12 +0200

    add module info

--- plugin.cpp
+++ plugin.cpp
@@ -1015,6 +1015,9 @@ public:
     {
         if(!registerScriptStuff())
             throw std::runtime_error("failed to register script stuff");
+
+        simSetModuleInfo(PLUGIN_NAME, 0, "OpenCV-based Image Processing Plugin", 0);
+        simSetModuleInfo(PLUGIN_NAME, 1, __DATE__, 0);
     }
 };
 
