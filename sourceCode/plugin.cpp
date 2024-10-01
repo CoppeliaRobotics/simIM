2f81167433a81ee0c402b3292f04ed06888babe1
Federico Ferri <federico.ferri.it@gmail.com>
2024-10-01 15:22:05 +0200

    fix scriptUid type

--- sourceCode/plugin.cpp
+++ sourceCode/plugin.cpp
@@ -66,7 +66,7 @@ public:
         setBuildDate(BUILD_DATE);
     }
 
-    void onScriptStateAboutToBeDestroyed(int scriptHandle, int scriptUid)
+    void onScriptStateAboutToBeDestroyed(int scriptHandle, long long scriptUid)
     {
         for(auto obj : matHandles.find(scriptHandle))
             delete matHandles.remove(obj);
