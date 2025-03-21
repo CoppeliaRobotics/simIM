6db3333ffa295023be4185d516deec6a87924954
Federico Ferri <federico.ferri.it@gmail.com>
2025-03-21 18:02:46 +0100

    quote strings inside @tag opts

--- lua/simIM.lua
+++ lua/simIM.lua
@@ -7,7 +7,7 @@ end
 
 -- @fun dataURL Encode image data according to "data" URL scheme (RFC 2397)
 -- @arg string imgHandle Handle to the image
--- @arg {type=string,default='BMP'} format Image format (BMP, JPG, or PNG)
+-- @arg {type='string',default='BMP'} format Image format (BMP, JPG, or PNG)
 -- @ret string output Buffer with encoded data
 function simIM.dataURL(imgHandle, fmt)
     local mime = {BMP = 'image/bmp', PNG = 'image/png', JPG = 'image/jpeg'}
